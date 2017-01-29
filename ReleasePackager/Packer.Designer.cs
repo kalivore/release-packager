33aa8bdd0d524105374488392f8444e7bb17e489
carbonel <carbonel@b3f044cf-877e-40fc-ad9c-bbe828048b5c>
2017-01-29 13:17:37 +0000

    git-svn-id: svn://reliquary:4000/share/svn/skyrim/ReleasePackager@128 b3f044cf-877e-40fc-ad9c-bbe828048b5c

---
+++ ReleasePackager/Packer.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace ReleasePackager
+{
+    partial class Packer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.tbGamePath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tbOutput = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.tbProgress = new System.Windows.Forms.TextBox();
+            this.tbArchiveName = new System.Windows.Forms.TextBox();
+            this.lblArchiveName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(83, 40);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(396, 20);
+            this.tbSource.TabIndex = 0;
+            this.tbSource.Text = "F:\\Games Work\\Skyrim\\Follower Potions\\mod";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(485, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(12, 43);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(65, 13);
+            this.lblSource.TabIndex = 2;
+            this.lblSource.Text = "Source path";
+            // 
+            // tbGamePath
+            // 
+            this.tbGamePath.Location = new System.Drawing.Point(83, 69);
+            this.tbGamePath.Name = "tbGamePath";
+            this.tbGamePath.Size = new System.Drawing.Size(396, 20);
+            this.tbGamePath.TabIndex = 0;
+            this.tbGamePath.Text = "G:\\Steam\\SteamApps\\common\\Skyrim";
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(485, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(12, 72);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(59, 13);
+            this.lblData.TabIndex = 2;
+            this.lblData.Text = "Game path";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(12, 101);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(63, 13);
+            this.lblOutput.TabIndex = 2;
+            this.lblOutput.Text = "Output path";
+            // 
+            // tbOutput
+            // 
+            this.tbOutput.Location = new System.Drawing.Point(83, 98);
+            this.tbOutput.Name = "tbOutput";
+            this.tbOutput.Size = new System.Drawing.Size(396, 20);
+            this.tbOutput.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(485, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button1";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnGo
+            // 
+            this.btnGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGo.Location = new System.Drawing.Point(452, 125);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(108, 43);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "GO";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbProgress.Location = new System.Drawing.Point(15, 174);
+            this.tbProgress.Multiline = true;
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbProgress.Size = new System.Drawing.Size(545, 417);
+            this.tbProgress.TabIndex = 3;
+            this.tbProgress.WordWrap = false;
+            // 
+            // tbArchiveName
+            // 
+            this.tbArchiveName.Location = new System.Drawing.Point(83, 12);
+            this.tbArchiveName.Name = "tbArchiveName";
+            this.tbArchiveName.Size = new System.Drawing.Size(396, 20);
+            this.tbArchiveName.TabIndex = 0;
+            this.tbArchiveName.Text = "Follower Potions";
+            // 
+            // lblArchiveName
+            // 
+            this.lblArchiveName.AutoSize = true;
+            this.lblArchiveName.Location = new System.Drawing.Point(12, 15);
+            this.lblArchiveName.Name = "lblArchiveName";
+            this.lblArchiveName.Size = new System.Drawing.Size(43, 13);
+            this.lblArchiveName.TabIndex = 2;
+            this.lblArchiveName.Text = "Archive";
+            // 
+            // Packer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 603);
+            this.Controls.Add(this.tbProgress);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblData);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblArchiveName);
+            this.Controls.Add(this.lblSource);
+            this.Controls.Add(this.tbGamePath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbOutput);
+            this.Controls.Add(this.tbArchiveName);
+            this.Controls.Add(this.tbSource);
+            this.Name = "Packer";
+            this.Text = "Mod Release Packer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.TextBox tbGamePath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox tbOutput;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox tbProgress;
+        private System.Windows.Forms.TextBox tbArchiveName;
+        private System.Windows.Forms.Label lblArchiveName;
+    }
+}
+
---
+++ ReleasePackager/Packer.cs
@@ -0,0 +1,365 @@
+﻿namespace ReleasePackager
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Windows.Forms;
+
+    public partial class Packer : Form
+    {
+        private string archiveName;
+        private string zipName;
+        private string modSourcePath;
+        private string tempDirRoot;
+        private string tempDirData;
+        private string manifestPath;
+        private string archiverPath;
+        private string compilerPath;
+        private string compilerFlagsPath;
+        private string coreScriptsSourcePath;
+        private string outputDirRoot;
+        private string zipSourceDir;
+
+        private const string ArchiverFilename = "Archive.exe";
+        private const string ArchiveBuilderFilename = "ArchiveBuilder.txt";
+        private const string ArchiveManifestFilename = "ArchiveManifest.txt";
+
+        public Packer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            archiveName = tbArchiveName.Text + ".bsa";
+            zipName = tbArchiveName.Text + ".zip";
+            modSourcePath = tbSource.Text;
+
+            compilerPath = Path.Combine(tbGamePath.Text, "Papyrus Compiler", "PapyrusCompiler.exe");
+            compilerFlagsPath = Path.Combine(tbGamePath.Text, "Papyrus Compiler", "TESV_Papyrus_Flags.flg");
+            coreScriptsSourcePath = Path.Combine(tbGamePath.Text, "Data", "Scripts", "Source");
+            archiverPath = Path.Combine(tbGamePath.Text, ArchiverFilename);
+
+            DoWorkNotOnUIThread();
+        }
+
+        private void DoWorkNotOnUIThread()
+        {
+            outputDirRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "packTest");
+            tempDirRoot = GetTemporaryDirectory();
+            tempDirData = Path.Combine(tempDirRoot, "Data");
+            manifestPath = Path.Combine(tempDirRoot, ArchiveManifestFilename);
+            zipSourceDir = Path.Combine(tempDirRoot, "ToZip");
+
+            CopyEsps();
+            CopySkse();
+
+            CopyScripts();
+            CompileScripts();
+
+            CopyOtherAssets();
+
+            CopyArchiver();
+            CreateArchiveBuilder();
+            CreateArchiveManifest();
+            BuildArchiveManifest();
+
+            CreateArchive();
+
+            CopyArchive();
+
+            CreateZip();
+
+            DeleteTemporaryDirectory();
+
+            AddProgress("ALL DONE! :D");
+        }
+
+        private void CopyEsps()
+        {
+            AddProgress("Copy ESP files");
+
+            CopyFiles(modSourcePath, zipSourceDir, "*.esp");
+
+            AddProgress("All ESPs copied");
+        }
+
+        private void CopySkse()
+        {
+            AddProgress("Copy SKSE folder");
+
+            var skseSource = Path.Combine(modSourcePath, "SKSE");
+            var skseDest = Path.Combine(zipSourceDir, "SKSE");
+            CopyDirectory(skseSource, skseDest, true);
+            
+            AddProgress("SKSE copied");
+        }
+
+        private void CopyScripts()
+        {
+            AddProgress("Copy Scripts source");
+
+            var sourcePath = Path.Combine(modSourcePath, "Scripts", "Source");
+            var destPath = Path.Combine(tempDirData, "Scripts", "Source");
+            CopyFiles(sourcePath, destPath, "*.psc");
+
+            AddProgress("All Scripts source copied");
+        }
+
+        private void CompileScripts()
+        {
+            AddProgress("Compile Scripts");
+
+            var exePath = $"\"{compilerPath}\"";
+            var scriptSourcePath = Path.Combine(tempDirData, "Scripts", "Source");
+            var compiledOutputPath = Path.Combine(tempDirData, "Scripts");
+            string args = $"\"{scriptSourcePath}\" -a -f=\"{compilerFlagsPath}\" -i=\"{scriptSourcePath};{coreScriptsSourcePath}\" -o=\"{compiledOutputPath}\"";
+
+            System.Diagnostics.Process papCompiler;
+            try
+            {
+                papCompiler = System.Diagnostics.Process.Start(exePath, args);
+                papCompiler.WaitForExit();
+            }
+            catch(Exception ex)
+            {
+                AddProgress("Script error! " + ex.Message);
+                throw;
+            }
+
+            AddProgress("All Scripts compiled");
+        }
+
+        private void CopyOtherAssets()
+        {
+            AddProgress("Copy Interface folder");
+            CopyDirectory(Path.Combine(modSourcePath, "Interface"), Path.Combine(tempDirData, "Interface"), true);
+            AddProgress("Interface folder copied");
+
+            AddProgress("Copy Seq folder");
+            CopyDirectory(Path.Combine(modSourcePath, "Seq"), Path.Combine(tempDirData, "Seq"), true);
+            AddProgress("Seq folder copied");
+
+            AddProgress("Copy Sound folder");
+            CopyDirectory(Path.Combine(modSourcePath, "Sound"), Path.Combine(tempDirData, "Sound"), true);
+            AddProgress("Sound folder copied");
+        }
+
+        private void CopyArchiver()
+        {
+            File.Copy(archiverPath, Path.Combine(tempDirRoot, ArchiverFilename), true);
+        }
+
+        private void CreateArchiveBuilder()
+        {
+            var builderPath = Path.Combine(tempDirRoot, ArchiveBuilderFilename);
+            using (StreamWriter sw = File.CreateText(builderPath))
+            {
+                sw.WriteLine("Log: " + "ArchiveLog.txt");
+                sw.WriteLine("New Archive");
+                //sw.WriteLine("Check: Meshes");
+                //sw.WriteLine("Check: Textures");
+                sw.WriteLine("Check: Menus");
+                sw.WriteLine("Check: Sounds");
+                sw.WriteLine("Check: Voices");
+                //sw.WriteLine("Check: Shaders");
+                //sw.WriteLine("Check: Trees");
+                //sw.WriteLine("Check: Fonts");
+                sw.WriteLine("Check: Misc");
+                sw.WriteLine("Check: Retain Directory Names");
+                sw.WriteLine("Check: Retain File Names");
+                //sw.WriteLine("Check: Compress Archive");
+                sw.WriteLine("Set File Group Root: " + "Data\\");
+                sw.WriteLine("Add File Group: " + ArchiveManifestFilename);
+                sw.WriteLine("Save Archive: " + archiveName);
+            }
+
+        }
+
+        private void CreateArchiveManifest()
+        {
+            var fs = File.Create(manifestPath);
+            fs.Close();
+        }
+
+        private void BuildArchiveManifest()
+        {
+            AddToManifest(tempDirData, "Interface", true);
+            AddToManifest(tempDirData, "Seq", true);
+            AddToManifest(tempDirData, "Sound", true);
+            AddToManifest(tempDirData, "Scripts", true);
+        }
+
+        private void CreateArchive()
+        {
+            var archiverPath = Path.Combine(tempDirRoot, ArchiverFilename);
+            var archiver = new System.Diagnostics.Process();
+            var startInfo = new System.Diagnostics.ProcessStartInfo
+            {
+                WorkingDirectory = tempDirRoot,
+                WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal,
+                FileName = archiverPath,
+                Arguments = ArchiveBuilderFilename,
+                UseShellExecute = false
+            };
+            try
+            {
+                archiver.StartInfo = startInfo;
+                archiver.Start();
+                archiver.WaitForExit();
+            }
+            catch (Exception ex)
+            {
+                AddProgress("Archiver error! " + ex.Message);
+                throw;
+            }
+
+            AddProgress("Archive created");
+        }
+
+        private void CopyArchive()
+        {
+            var archivePath = Path.Combine(tempDirRoot, archiveName);
+            File.Copy(archivePath, Path.Combine(zipSourceDir, archiveName), true);
+        }
+
+        private void CreateZip()
+        {
+            var zipPath = Path.Combine(outputDirRoot, zipName);
+            if (File.Exists(zipPath))
+            {
+                File.Delete(zipPath);
+            }
+
+            try
+            {
+                ZipFile.CreateFromDirectory(zipSourceDir, zipPath);
+            }
+            catch (Exception ex)
+            {
+                AddProgress("Zipping error! " + ex.Message);
+                throw;
+            }
+
+            AddProgress("Zip created");
+        }
+
+        private void DeleteTemporaryDirectory()
+        {
+            Directory.Delete(tempDirRoot, true);
+
+            AddProgress("Temp directory removed");
+        }
+
+
+        private string GetTemporaryDirectory()
+        {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            return tempDirectory;
+        }
+
+        private void CopyFiles(string sourceDirName, string destDirName, string filePattern)
+        {
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            string[] files;
+            try
+            {
+                files = Directory.GetFiles(sourceDirName, filePattern);
+            }
+            catch (Exception ex)
+            {
+                AddProgress("Error copying files: " + ex.Message);
+                return;
+            }
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                var filename = Path.GetFileName(files[i]);
+                File.Copy(Path.Combine(sourceDirName, filename), Path.Combine(destDirName, filename), true);
+                AddProgress($"Copied {filename}");
+            }
+        }
+
+        private void CopyDirectory(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            if (!dir.Exists)
+            {
+                AddProgress("No directory to copy");
+                return;
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    CopyDirectory(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+
+            AddProgress($"Copied {sourceDirName}");
+        }
+
+        private void AddToManifest(string rootName, string usedPath, bool recurse)
+        {
+            var sourceDirName = Path.Combine(rootName, usedPath);
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+            if (!dir.Exists)
+            {
+                AddProgress("No directory to copy");
+                return;
+            }
+
+            using (StreamWriter sw = File.AppendText(manifestPath))
+            {
+                // Get the files in the directory and copy them to the new location.
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    string filepath = Path.Combine(usedPath, file.Name);
+                    sw.WriteLine(filepath);
+                    AddProgress($"Added {filepath} to manifest");
+                }
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (recurse)
+            {
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(usedPath, subdir.Name);
+                    AddToManifest(rootName, temppath, recurse);
+                }
+            }
+
+        }
+
+        private void AddProgress(string progress)
+        {
+            tbProgress.AppendText(Environment.NewLine + progress);
+        }
+    }
+}
